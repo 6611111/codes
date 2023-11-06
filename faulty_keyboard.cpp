cbe13a3562fa59d9b69eec083757ba2328d8d649
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2023-11-06 19:58:24 +0530

    faulty_keyboard.cpp

---
+++ faulty_keyboard.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string res;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!='i') res+=s[i];
+            if(s[i]=='i') reverse(res.begin(),res.end());
+        }
+        return res;
+    }
+};

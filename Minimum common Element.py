d2132e66c4c69687bdba9bec97de9f250e91c5bd
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2024-03-09 16:03:46 +0530

    Create Minimum common Element.py

---
+++ Minimum common Element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        s1=set( nums1)
+        s2=set(nums2)
+        s3=s1.intersection(s2)
+        if(len(s3)==0):
+            return -1
+        return min(s3)

ea11283cab0cde568b104916ca9b119334e932c9
Anusha Yelleti <119839788+6611111@users.noreply.github.com>
2024-03-13 11:49:56 +0530

    Create find the pivot integer.cpp

---
+++ find the pivot integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum1=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum1+=i;
+            int sum2=0;
+            for(int j=i;j<=n;j++)
+            {
+                sum2+=j;
+            }
+            if(sum1==sum2)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
